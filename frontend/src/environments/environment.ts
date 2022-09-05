a4a962584207d13f5cc55282e7165cfab35e5581
Pablo Sgreccia <pablosgreccia@gmail.com>
2022-09-05 08:47:09 -0300

    add: heroku host url

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,8 +4,12 @@
 
 export const environment = {
   production: false,
-  HOST:'http://localhost:3000'
+  // Local host
+  //HOST:'http://localhost:3000'
+  // Mobile host
   //HOST:'http://192.168.0.205:3000'
+  // Heroku host
+  HOST:'http://eventoslasegunda.herokuapp.com'
 };
 
 /*
